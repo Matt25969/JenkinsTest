70b6647e9b5f3080fd9ff29afb79b79a9515b244
Matt25969 <matt.jo.hunt@gmail.com>
2018-04-04 08:59:46 +0100

    fixed and comeplete - good jumping off point

--- CQC Repo/src/test/java/CQC.java
+++ CQC Repo/src/test/java/CQC.java
@@ -13,6 +13,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.*;
 import org.junit.*;
+import org.junit.internal.runners.statements.Fail;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -142,7 +143,7 @@ public class CQC {
         WebElement loginMessage = driver.findElement(By.cssSelector(
                 "body > table > tbody > tr > td.auto-style1 > big > blockquote > blockquote > font > center > b"));
 
-        assertEquals(loginMessage.getText(), "**Successful Login*");
+        assertEquals(loginMessage.getText(), "**Successful Login**");
 
         if (loginMessage.getText().equals("**Successful Login**")) {
             test.log(LogStatus.PASS, "Login valid");
